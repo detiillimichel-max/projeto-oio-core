d696e0501d8154e75c9706771378eb9f4a62c4e9
Michel Detiilli <detiillimichel@gmail.com>
2026-03-16 12:20:48 -0300

    Update sw.js
    O Soldador do Sistema

--- sw.js
+++ sw.js
@@ -1,10 +1,21 @@
-const CACHE_NAME = 'oio-v5-cache-v1';
-const assets = ['teste.html', 'videos.html', 'games.html', 'icone-512.png', 'manifest.json'];
+const CACHE_NAME = 'oio-one-v5';
+const assets = [
+  'index.html',
+  'vibe_connect.html',
+  'vibe_hub.html',
+  'games.html',
+  'teste.html',
+  'vibe.html',
+  'portal.html',
+  'vibe_som.html',
+  'manifest.json',
+  'icone-512.png'
+];
 
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(res => res || fetch(event.request)));
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
